efd93f9ccdb40b511570235ad4cd6e5ab5d529fb
Alex Adams <2106870@chester.ac.uk>
2026-06-01 15:17:38 +0100

    Commit 2 Open Weather API Implementation.
    Updated MainActivity.kt to connect to the API using a key linked to Open Weather AI.
    
    - Added method, getWeather: Connects to the API URL and gathers input/output data with error handling.
    
    - Added method onPostExecute, Extracts the JSON weather data to regular strings and assigning them to weather variables to allow data display for selected city and country (for now, this will be for London, UK).
    
    - Updated onCreate to use the getWeatherData method to update the weather app, this will be ideal for further in development when the user wants to input a different location.
    
    - Granted android permission INTERNET in the AndroidManifest.xml file.

--- app/src/main/java/com/example/androidweather/MainActivity.kt
+++ app/src/main/java/com/example/androidweather/MainActivity.kt
@@ -5,35 +5,87 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.androidweather.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val APIkey: String = "b57f2441e36a0d4d23953cadfbfe78c9"
 
-    private var APIkey: String = "b57f2441e36a0d4d23953cadfbfe78c9"
+    private var city: String = "London"
+    private var countryCode: String = "UK"
+
+    private var location: String = "$city, $countryCode"
+
+
+    //Used to call the API and get the weather data using the city name and & API key
+    private fun getWeatherData(){
+        lifecycleScope.launch { //Creates a coroutin while app makes request for weather data
+            try{
+            val result = withContext(Dispatchers.IO) { //Runs a thread for an input/output call for API data.
+                //searches with the URL provided.                                  (readText(Charsets.UTF_8) is used so different text characters are recognised while making this request.
+                URL("https://api.openweathermap.org/data/2.5/weather?q=$location&units=metric&appid=$APIkey").readText(Charsets.UTF_8)
+            }
+                onPostExecute(result)
+
+        }
+        catch (e: Exception){
+            e.printStackTrace() //For if the API request fails to connect.
+            //todo:: Implement feature to notify users in app when this happens (For example: When theres no internet.)
+        }
+
+        }
+    }
+
+    fun onPostExecute(result: String?){
+
+        try{
+            val jsonObj = JSONObject(result)
+            val main = jsonObj.getJSONObject("main")
+            val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+            val temp = main.getString("temp")
+            val humidity = main.getString("humidity")
+            val wind = jsonObj.getJSONObject("wind")
+
+            val weatherStatus = weather.getString("description")
+            binding.weatherText.text = weatherStatus
+            binding.tempText.text = "$temp °C"
+
+            val windString = wind.getString("speed")
+            binding.windText.text = "$windString m/s"
+            binding.humidityText.text = humidity
+        }
+        catch (e: Exception){
+            e.printStackTrace() //sort later
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
+        //Standard start up onCreate code.
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        //setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        
+        binding.cityText.text = location
+        getWeatherData()
+
+
         //Searches the city when the user enters the name.
         binding.btnSearch.setOnClickListener {
-
+            //TODO:: implement search feature.
         }
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }

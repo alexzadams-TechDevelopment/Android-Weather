384e5fdbfb65d932086d0a04f09e79062f2ee787
alexzadams-TechDevelopment <alexzadamswork@gmail.com>
2026-08-24 22:41:10 +0100

    Commit 9: Updating UI progress 2
    - Backgrounds in activity_main.xml now change depending on the time of day using code in MainActivity.kt
       - dayTime Boolean statement added to remain true unless the swtich statement is active from a night call, which then becomes false.
    
    - New method guiColour that changes UI elements to blue during the day & purple at night.
      - Applies to...
        1) info_box2.xml
        2) fade.xml
        3) btnSearch
        4) activity_main.xml
    
    - Added xml asset files...
      1) spinner_box.xml (changes the font color, may change the background, length & radius in future)
    
    - changed btnSearch element to a MaterialButton to implement colour functionality that changes with the time of day of zone selected.
    
    To do:
    - Finalise GUI design for different app compatibility, layout changes & change to font sizes.
    
    - May focus on working on the App to get both the longitude and latitude on the request based on the user's location.
    
    - Remember the location for next time or have it pre-set to user's location (see point above).
    
    -? May add more weather data (Scrapped, will be added after project is completed).

--- app/src/main/java/com/example/androidweather/MainActivity.kt
+++ app/src/main/java/com/example/androidweather/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.androidweather
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -18,6 +20,7 @@ import android.widget.VideoView
 import com.google.android.gms.location.FusedLocationProviderClient
 import java.util.Locale
 import android.net.Uri
+import android.graphics.drawable.GradientDrawable
 
 
 class MainActivity : AppCompatActivity() {
@@ -81,6 +84,8 @@ class MainActivity : AppCompatActivity() {
             binding.windText.text = "$windString m/s"
             binding.humidityText.text = humidity
 
+            var dayTime: Boolean = true // To declare if the area searched is day or not using the switch statement below.
+
             //Updates the image on display
             val weatherIcon = when(icon){ //TODO:: Replace the current images with my own design (current is from open weather AI as a place holder)
                 //Day
@@ -95,19 +100,21 @@ class MainActivity : AppCompatActivity() {
                 "50d" -> {weatherMedia = R.raw.fog_day; R.drawable.d50}
 
                 //Night
-                "01n" -> {weatherMedia = R.raw.night; R.drawable.n01}
-                "02n" -> {weatherMedia = R.raw.cloudy_night; R.drawable.n02}
-                "03n" -> {weatherMedia = R.raw.cloudy_night; R.drawable.n03}
-                "04n" -> {weatherMedia = R.raw.cloudy_night; R.drawable.n04}
-                "09n" -> {weatherMedia = R.raw.rain_night; R.drawable.n09}
-                "10n" -> {weatherMedia = R.raw.cloudy_night; R.drawable.n10}
-                "11n" -> {weatherMedia = R.raw.thunder_night; R.drawable.n11}
-                "13n" -> {weatherMedia = R.raw.snow_night; R.drawable.n13}
-                "50n" -> {weatherMedia = R.raw.cloudy_night; R.drawable.n50} //update to more suitable video
+                "01n" -> {dayTime = false; weatherMedia = R.raw.night; R.drawable.n01;}
+                "02n" -> {dayTime = false; weatherMedia = R.raw.cloudy_night; R.drawable.n02;}
+                "03n" -> {dayTime = false; weatherMedia = R.raw.cloudy_night; R.drawable.n03;}
+                "04n" -> {dayTime = false; weatherMedia = R.raw.cloudy_night; R.drawable.n04;}
+                "09n" -> {dayTime = false; weatherMedia = R.raw.rain_night; R.drawable.n09;}
+                "10n" -> {dayTime = false; weatherMedia = R.raw.cloudy_night; R.drawable.n10;}
+                "11n" -> {dayTime = false; weatherMedia = R.raw.thunder_night; R.drawable.n11;}
+                "13n" -> {dayTime = false; weatherMedia = R.raw.snow_night; R.drawable.n13;}
+                "50n" -> {dayTime = false; weatherMedia = R.raw.cloudy_night; R.drawable.n50;} //update to more suitable video
 
                 else -> {R.drawable.unknown; R.raw.sun_cloudy_day}
             }
             binding.weatherImageView.setImageResource(weatherIcon)
+
+
 
             //Play video
             val video = findViewById<VideoView>(R.id.videoBackground) //Selects the videoView entity in the activity file.
@@ -118,9 +125,38 @@ class MainActivity : AppCompatActivity() {
                 it.isLooping = true
                 video.start()
             }
+
+
+            //To update box colours.
+            guiColour(dayTime)
         }
         catch (e: Exception){
             e.printStackTrace() //sort later
+        }
+    }
+
+    // Used to change the colour of elements in GUI depending on the day & night cycle of the searched area.
+    private fun guiColour(dayTime: Boolean){
+
+        val backgroundBox = binding.infoBox2.background.mutate() as GradientDrawable
+        val backgroundFade = binding.fade.background.mutate() as GradientDrawable
+
+        if(dayTime){ //Day Time (blue)
+            binding.root.setBackgroundColor(Color.parseColor("#41B1E0"))
+            backgroundBox.setColor(Color.parseColor("#41b1e0"))
+            backgroundFade.colors = intArrayOf(Color.parseColor("#41b1e0"), Color.TRANSPARENT)
+
+            binding.btnSearch.setBackgroundColor(Color.parseColor("#28718F"))
+            binding.btnSearch.setTextColor(Color.WHITE)
+
+
+        }else{ //Night Time (purple)
+            binding.root.setBackgroundColor(Color.parseColor("#7731B5"))
+            backgroundBox.setColor(Color.parseColor("#7731B5"))
+            backgroundFade.colors = intArrayOf(Color.parseColor("#7731B5"), Color.TRANSPARENT)
+
+            binding.btnSearch.setBackgroundColor(Color.parseColor("#2F154A"))
+            binding.btnSearch.setTextColor(Color.WHITE)
         }
     }
 
@@ -175,7 +211,7 @@ class MainActivity : AppCompatActivity() {
         //Spinner Set up//
         val adapter = ArrayAdapter(
             this,
-            android.R.layout.simple_spinner_item,
+            R.layout.spinner_box, //Now uses spinner_Box.xml design in the layout File.
             countries
         )
 
